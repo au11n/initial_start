4219a0a92e577be49206838bbaa9ae215064adc9
natan_gard <natangard@gmail.com>
2023-07-18 21:20:06 +0400

    minor update

--- start_ubuntu.sh
+++ start_ubuntu.sh
@@ -57,14 +57,13 @@ pip install jupyterlab
 #Krop PDF
 sudo snap install krop
 
-#для работы ifconfig
 sudo apt install net-tools
 
-#Sublime тестовый редактор
+#Sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
 
-#показать ключи ssh
+#show ssh_key
 cat ~/.ssh/id_rsa.pub 
