c88f49a5ac048b71263d38bc6483d9c011a47378
natangard <natangard@gmail.com>
2024-06-09 14:39:55 +0400

    update scripts

--- start.sh
+++ start.sh
@@ -6,14 +6,7 @@ sudo apt update && sudo apt upgrade && sudo apt autoremove && sudo apt autopurge
 sudo apt install -y vlc
 
 #for coding
-sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git micro
-
-#for CS Source:)
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
-sudo apt-get install libtcmalloc-minimal4:i386
-sudo apt-get install libgl1-mesa-glx:i386
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev git
 
 #venv
 sudo apt install python3-venv
@@ -34,8 +27,9 @@ sudo flatpak install flathub org.telegram.desktop
 sudo apt install tilix
 
 #text_editor
-sudo apt install kate
-sudo apt install breeze
+sudo apt install kate breeze
+
+sudo apt install micro
 # micro ~/.config/micro/settings.json
 # {
 #     "hlsearch": true,
@@ -60,3 +54,7 @@ sudo apt install syncthing
 
 #keepassxc
 sudo apt install keepassxc
+
+#CPU
+# @reboot sleep 30 && sudo cpupower frequency-set -g powersave -d 800kHz -u 1.2GHz
+# sudo crontab -u root -e
--- sync_flashdrive.sh
+++ sync_flashdrive.sh
@@ -15,15 +15,18 @@ mkdir -p "$CONFIG_DIR" "$DOCUMENTS_DIR" "$COD_DIR" "$MUSIC_DIR" "$PICTURES_DIR"
 # Определение домашнего каталога пользователя
 SOURCE_DIR="$HOME"
 
-# Синхронизация папок и файлов
-rsync -avu --delete --progress "$SOURCE_DIR/Documents/" "$DOCUMENTS_DIR/"
+# Синхронизация папок и файлов с исключением служебных папок в 'cod'
+rsync -avu --delete --progress --exclude 'excluded_folder/' "$SOURCE_DIR/Documents/" "$DOCUMENTS_DIR/"
 rsync -avu --delete --progress "$SOURCE_DIR/Music/" "$MUSIC_DIR/"
-rsync -avu --delete --progress "$SOURCE_DIR/cod/" "$COD_DIR/"
+rsync -avu --delete --progress --exclude 'venv/' --exclude '.git/' --exclude '.ipynb_checkpoints/' --exclude 'lib/' --exclude 'hist_data/' "$SOURCE_DIR/cod/" "$COD_DIR/"
 rsync -avu --delete --progress "$SOURCE_DIR/Pictures/" "$PICTURES_DIR/"
 
 rsync -avu --delete --progress "$SOURCE_DIR/.ssh/config" "$CONFIG_DIR/"
 rsync -avu --delete --progress "$SOURCE_DIR/.bash_aliases" "$CONFIG_DIR/"
 rsync -avu --delete --progress "$SOURCE_DIR/.bash_history" "$CONFIG_DIR/"
 rsync -avu --delete --progress "$SOURCE_DIR/.bashrc" "$CONFIG_DIR/"
+
+# Синхронизация файла sync_flashdrive.sh
+rsync -avu --progress "$SOURCE_DIR/cod/initial_start/sync_flashdrive.sh" "$TARGET_DIR/"
 
 echo "Синхронизация завершена."
